a42b97b71672bc8dc0fab4bbffc021acc8da1db3
David Vodzinskyi <nikeron2002@gmail.com>
2025-07-23 01:52:49 +0300

    header intro half

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 "use strict";
 
-const swiper = new Swiper('.about__slider', {
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 40,
-    navigation: {
-      nextEl: '.about__slide-next',
-      prevEl: '.about__slide-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-      },
-    breakpoints: {
-        1200: {
-        //    slidesPerView: 'auto',
-        //    spaceBetween: 40,
-        },
-        830: {
-        //    slidesPerView: 'auto',
-        }
-     },
-  });
+const swiper = new Swiper(".intro__swiper", {
+  loop: true,
+  autoplay: {
+    delay: 500,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 50,
+  slidesPerView: 5,
+  slidesPerGroup: 1,
+   watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  // navigation: {
+  //   nextEl: ".about__slide-next",
+  //   prevEl: ".about__slide-prev",
+  // },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // // },
+  // breakpoints: {
+  //   1200: {
+  //     //    slidesPerView: 'auto',
+  //     //    spaceBetween: 40,
+  //   },
+  //   830: {
+  //     //    slidesPerView: 'auto',
+  //   },
+  // },
+});
