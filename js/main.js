9990b45b942c4dbb8f6a7091dfd66a378f59c93e
Lapulya19 <sveta.serdyuk2003@gmail.com>
2025-07-23 17:08:43 +0300

    slider fix, intro,about,flow, partners styles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 "use strict";
-
-const swiper = new Swiper(".intro__swiper", {
-  loop: true,
-  autoplay: {
-    delay: 500,
-    disableOnInteraction: false,
-  },
-  spaceBetween: 50,
-  slidesPerView: 5,
-  slidesPerGroup: 1,
-   watchSlidesProgress: true,
-  watchSlidesVisibility: true,
+ const swiper = new Swiper(".intro__swiper", {
+    slidesPerView: 'auto',
+      spaceBetween: 48,
+      freeMode: true,
+      speed: 3000,
+      allowTouchMove: false,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+        },
+        640: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 5,
+        },
+      },
   // navigation: {
   //   nextEl: ".about__slide-next",
   //   prevEl: ".about__slide-prev",
